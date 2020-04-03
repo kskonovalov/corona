eed45dc6976f94c50cba8ba2b3c14f06138e32d7
Konstantin <picprofit@gmail.com>
2020-04-03 19:04:06 +0300

    add: graph first view

--- src/pages/Index/Index.tsx
+++ src/pages/Index/Index.tsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import csv from 'csvtojson';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from 'recharts';
 
 const apiUrl =
   'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
 
 const Index = () => {
-  const [data, setData] = useState<object[]>([]);
-  const [filteredData, setFilteredData] = useState<object[]>([]);
+  const [data, setData] = useState<any>([]);
+  const [graphData, setGraphData] = useState<any>([]);
 
   //  get data from api
   useEffect(() => {
@@ -17,27 +26,60 @@ const Index = () => {
         output: 'json'
       })
         .fromString(apiData)
-        .then((jsonData: object[]) => {
+        .then((jsonData: any) => {
           setData(jsonData);
         });
     });
   }, []);
 
   useEffect(() => {
-    if(data.length === 0) {
+    if (data.length === 0) {
       return;
     }
-    setFilteredData(data.filter((item: any) => {
-      return item["Country/Region"] === "Russia";
-    }))
+    const filteredData = data.filter((item: any) => {
+      return item['Country/Region'] === 'Russia';
+    });
+    const countryData = filteredData[0];
+    delete countryData["Province/State"];
+    delete countryData["Country/Region"];
+    delete countryData["Lat"];
+    delete countryData["Long"];
+    setGraphData(Object.keys(countryData).map((date: string) => {
+      return {
+        date,
+        infected: countryData[date]
+      };
+    }));
+
   }, [data]);
 
-  console.log(filteredData);
+  console.log();
 
   return (
     <div>
       <h1>Hello there!</h1>
-      <pre>{}</pre>
+      <LineChart
+        width={800}
+        height={400}
+        data={graphData}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <XAxis dataKey="name" />
+        <YAxis dataKey="infected" />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="infected"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
     </div>
   );
 };
