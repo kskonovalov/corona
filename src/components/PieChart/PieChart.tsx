eb45c7d0277e965061135964d4ea22b36214a57d
Konstantin <picprofit@gmail.com>
2020-04-04 12:56:00 +0300

    upd: random color for pie

--- src/components/PieChart/PieChart.tsx
+++ src/components/PieChart/PieChart.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { PieChart as PieChartGraph, Pie, Tooltip } from 'recharts';
+import { PieChart as PieChartGraph, Pie, Tooltip, Cell } from 'recharts';
 import axios from 'axios';
-import csv from 'csvtojson';
+
+import { getRandomColor } from '../../helpers';
 
 const MyComponent = () => {
   const apiUrl = 'https://kskonovalov.me/samples/corona/api/';
@@ -11,12 +12,16 @@ const MyComponent = () => {
     axios.get(apiUrl).then(res => {
       const { data: apiData } = res;
       setData(
-        apiData.map((item: any) => {
-          return {
-            ...item,
-            sick: +item.sick
-          };
-        })
+        apiData
+          .filter((item: any) => {
+            return item.sick > 30;
+          })
+          .map((item: any) => {
+            return {
+              ...item,
+              sick: +item.sick
+            };
+          })
       );
     });
   }, []);
@@ -31,14 +36,18 @@ const MyComponent = () => {
         nameKey="title"
         dataKey="sick"
         data={data}
-        cx={250}
+        cx={240}
         cy={250}
-        outerRadius={150}
+        outerRadius={130}
         fill="#8884d8"
         label={({ name, sick }) => {
-          return sick > 100 ? name : '';
+          return `${name} (${sick})`;
         }}
-      />
+      >
+        {data.map((item: any, i: number) => (
+          <Cell key={`cell-${i}`} fill={getRandomColor()} />
+        ))}
+      </Pie>
       <Tooltip />
     </PieChartGraph>
   );
