27b3939e38a54e6a7da97f10a3a8bd03f029f326
Konstantin <picprofit@gmail.com>
2020-04-05 10:56:36 +0300

    rm: old removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import Index from './pages/Index';
+import LineChart from './components/LineChart';
+import PieChart from './components/PieChart';
 
+const StyledIndex = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  & > div {
+    width: 45%;
+    min-width: 500px;
+  }
+`;
 function App() {
   return (
     <div className="App">
-        <h1>Russia infected covid-19 count</h1>
-      <Index />
+      <h1>Russia infected covid-19 count</h1>
+      <StyledIndex>
+        <div>
+          <h2>Total infected in Russia</h2>
+          <LineChart />
+        </div>
+        <div>
+          <h2>Infected in Russia by areas on today</h2>
+          <PieChart />
+        </div>
+      </StyledIndex>
     </div>
   );
 }
