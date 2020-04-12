12be53b9c37b3ee8c7960cbf92a94623295f3216
Konstantin <picprofit@gmail.com>
2020-04-12 19:26:18 +0300

    upd: menu styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledMenu = styled.nav`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const StyledLink = styled(NavLink)`
+  padding: 5px 10px;
+  text-decoration: none;
+  border-right: 1px solid #8bd390;
+  &:hover {
+    text-decoration: underline;
+  }
+  &:last-child {
+    border: 0;
+  }
+`;
 
 const Header = () => {
   return (
     <>
       <h1>Covid 2020 visual</h1>
-      <NavLink to="/">World</NavLink>
-      <NavLink to="/russia">Russia</NavLink>
+      <StyledMenu>
+        <StyledLink to="/">World</StyledLink>
+        <StyledLink to="/russia">Russia</StyledLink>
+      </StyledMenu>
     </>
   );
 };
