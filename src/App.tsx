4b9b421dedc4fae3d9e9c45c5ef27c4be37aebbe
Konstantin <picprofit@gmail.com>
2020-04-13 18:27:14 +0300

    add: menu styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import styled from 'styled-components';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
 
 import Header from './components/Header/Header';
 import CovidGLobal from './Pages/CovidGlobal/CovidGlobal';
 import CovidRussianAreas from './Pages/CovidRussianAreas/CovidRussianAreas';
 
-const StyledIndex = styled.section`
-  width: 1000px;
-  max-width: 100%;
-  margin: 0 auto;
-`;
-
 function App() {
   return (
-    <StyledIndex>
+    <Container>
       <BrowserRouter>
         <Header />
         <Route path="/" exact component={CovidGLobal} />
         <Route path="/russia" component={CovidRussianAreas} />
       </BrowserRouter>
-    </StyledIndex>
+    </Container>
   );
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
-
-const StyledMenu = styled.nav`
-  display: flex;
-  flex-wrap: wrap;
-`;
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 const StyledLink = styled(NavLink)`
-  padding: 5px 10px;
   text-decoration: none;
-  border-right: 1px solid #8bd390;
-  &:hover {
-    text-decoration: underline;
-  }
-  &:last-child {
-    border: 0;
-  }
 `;
 
 const Header = () => {
   return (
     <>
       <h1>Covid 2020 visual</h1>
-      <StyledMenu>
-        <StyledLink to="/">World</StyledLink>
-        <StyledLink to="/russia">Russia</StyledLink>
-      </StyledMenu>
+      <ButtonGroup color="primary" aria-label="outlined primary button group">
+        <Button>
+          <StyledLink to="/">World</StyledLink>
+        </Button>
+        <Button>
+          <StyledLink to="/russia">Russia</StyledLink>
+        </Button>
+      </ButtonGroup>
     </>
   );
 };
