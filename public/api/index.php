cde2012b47fd31b04b5211fbec4aad983d7df313
Konstantin <picprofit@gmail.com>
2020-04-18 10:52:22 +0300

    upd: api regex and empty check

--- public/api/index.php
+++ public/api/index.php
@@ -19,11 +19,10 @@ curl_setopt($ch, CURLOPT_URL,"https://xn--80aesfpebagmfblc0a.xn--p1ai/");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $server_output = curl_exec($ch);
 curl_close ($ch);
-
-preg_match("#\<script\>var mapData = (.*?);</script>#smi", $server_output, $result);
-
+preg_match("#var mapData = (.*?);</script>#smi", $server_output, $result);
 $mapData = $result[1];
-
-file_put_contents($file, $mapData);
+if(!empty($mapData)) {
+    file_put_contents($file, $mapData);
+}
 
 echo $mapData;
