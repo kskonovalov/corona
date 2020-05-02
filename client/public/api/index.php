518baec45386b917226ef3aa2a64176c2da76504
Konstantin <picprofit@gmail.com>
2020-05-02 19:57:04 +0300

    upd: api fix

--- client/public/api/index.php
+++ client/public/api/index.php
@@ -18,11 +18,11 @@ if (file_exists($file)) {
 }
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,"https://xn--80aesfpebagmfblc0a.xn--p1ai/");
+curl_setopt($ch, CURLOPT_URL,"https://xn--80aesfpebagmfblc0a.xn--p1ai/information/");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $server_output = curl_exec($ch);
 curl_close ($ch);
-preg_match("#\:map-data=\'(.*?)'>#smi", $server_output, $result);
+preg_match("#\:spread-data=\'(.*?)'#smi", $server_output, $result);
 $mapData = $result[1];
 if(!empty($mapData)) {
     file_put_contents($file, $mapData);
