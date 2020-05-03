fb3adf5f5aed05754fb1c7c2a8f1bf576dd873a1
Konstantin <picprofit@gmail.com>
2020-05-03 15:45:54 +0300

    add: min count filter

--- client/src/pages/CovidRussianAreas.tsx
+++ client/src/pages/CovidRussianAreas.tsx
@@ -1,33 +1,82 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import axios from 'axios';
+import { FormControl, TextField, Typography } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { deepOrange } from '@material-ui/core/colors';
 
 import PieChart from '../components/PieChart';
-import axios from "axios";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    padded: {
+      padding: '7px 5px'
+    },
+    outlined: {
+      borderColor: deepOrange[300]
+    },
+    inputWrap: {
+      margin: '0 10px',
+      position: 'relative',
+      width: '150px',
+      '& .MuiInput-underline:after': {
+        borderColor: deepOrange[300]
+      }
+    },
+    input: {
+      fontSize: '2.125rem',
+      textAlign: 'center',
+      marginTop: '-11px'
+    }
+  })
+);
 
 const CovidRussianAreas = () => {
-    const [data, setData] = useState<object[]>([]);
-    const [minCount, setMinCount] = useState<number>(1000);
+  const classes = useStyles();
 
-    const apiUrl = 'http://localhost:5000/api/russia-areas';
-    useEffect(() => {
-        axios.post(apiUrl, {
-            minCount: 1000
-        }).then(res => {
-            const { data: apiData } = res.data;
-            console.log(apiData);
-            if(apiData.length > 0) {
-                setData(
-                    apiData
-                );
-            }
-        });
-    }, []);
+  const [data, setData] = useState<object[]>([]);
+  const [minCount, setMinCount] = useState<number>(1000);
 
-    return (
-        <>
-            <h2>Infected in Russia by areas on today</h2>
-            <PieChart data={data} />
-        </>
-    );
+  const apiUrl = 'http://localhost:5000/api/russia-areas';
+  useEffect(() => {
+    axios
+      .post(apiUrl, {
+        minCount
+      })
+      .then(res => {
+        const { data: apiData } = res.data;
+        console.log(apiData);
+        if (apiData.length > 0) {
+          setData(apiData);
+        }
+      });
+  }, [minCount]);
+
+  return (
+    <>
+      <h2>Infected in Russia by areas on today</h2>
+      <Typography variant="h4" component="h2">
+        Minimum count of infected to display:
+        <FormControl variant="outlined">
+          <TextField
+            value={minCount}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              setMinCount(parseInt(event.currentTarget.value) || 0);
+            }}
+            classes={{
+              root: classes.padded
+            }}
+            InputProps={{
+              classes: {
+                input: classes.input
+              }
+            }}
+            className={classes.inputWrap}
+          />
+        </FormControl>
+      </Typography>
+      <PieChart data={data} />
+    </>
+  );
 };
 
 export default CovidRussianAreas;
